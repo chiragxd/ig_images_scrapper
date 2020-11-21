cfb838c5a1fb1b5df33d2cc826f96ec1026b6c95
Kishan Modi <45737372+kishanmodi@users.noreply.github.com>
2020-11-21 09:35:31 +0530

    commit

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ chrome_options = Options()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("GOgitOGLE_CHROME_BIN"),chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ.get("GOOGLE_CHROME_BIN"),chrome_options=chrome_options)
 
 
 bot_token ="1471730304:AAGqL9p0aUBstl3q9qEV1GxRa2BbJ4DaD08"
