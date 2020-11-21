ac811c3c419abee3f481e00b01dff2b0ed82166b
Kishan Modi <45737372+kishanmodi@users.noreply.github.com>
2020-11-21 08:13:00 +0530

    make it better

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ from selenium.webdriver.chrome.options import Options
 from flask import Flask, request
 
 chrome_options = Options()
-chrome_options.headless = True
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
